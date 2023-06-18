08e5e0c2330b920d68299a457c4d73cfb28e21cb
Raphile14 <rllddalangin@gmail.com>
2023-06-19 07:47:33 +0800

    feat: modified getProjects to use responseMiddleware

--- backend/src/application/rest/ProjectController.ts
+++ backend/src/application/rest/ProjectController.ts
@@ -3,8 +3,8 @@ import path from "path";
 
 // TODO: Implement Get List of Projects
 export const getProjects = async (req, res) => {
-  return res.status(200).json({
-    message: "Get List of Projects",
+  return res.sendSuccess({
+    projects: [],
   });
 };
 
