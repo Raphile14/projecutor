102d92ad7211c57ac55aa993674029e5acc7a000
Raphile14 <rllddalangin@gmail.com>
2023-06-19 07:26:36 +0800

    feat: added cloneRepository to routes

--- backend/src/application/route/ProjectRoute.ts
+++ backend/src/application/route/ProjectRoute.ts
@@ -1,8 +1,9 @@
 import express from "express";
-import { getProjects } from "../rest/ProjectController";
+import { getProjects, cloneRepository } from "../rest/ProjectController";
 const router = express.Router();
 
 // Controllers
 router.get("/", getProjects);
+router.post("/clone", cloneRepository);
 
 export default router;
